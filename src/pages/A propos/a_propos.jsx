800139a72297f7de268c3a5dc28257d2cdb9acc4
BenjaminJedar <benjisaintpaul@hotmail.fr>
2023-12-17 00:59:23 +0100

    Premiere mise en place du responsive

--- src/pages/A propos/a_propos.jsx
+++ src/pages/A propos/a_propos.jsx
@@ -1,8 +1,36 @@
+import './a_propos.css';
+import ana_img from '../../assets/Ana.jpeg';
+
 function A_propos() {
   return (
-    <div>
-      Voici la merveilleuse Ana super forte en Yoga et en plus elle est vraiment
-      trop mignonne !
+    <div className="a_propos">
+      <img src={ana_img} alt="La belle Ana" className="ana_img" />
+      <h2>Salut moi c'est Ana ...</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. In tellus integer
+        feugiat scelerisque varius. Aenean vel elit scelerisque mauris
+        pellentesque pulvinar. Leo urna molestie at elementum eu. Morbi quis
+        commodo odio aenean sed adipiscing diam donec adipiscing. Tellus mauris
+        a diam maecenas sed enim ut sem viverra. Nunc pulvinar sapien et ligula
+        ullamcorper malesuada. Morbi leo urna molestie at elementum eu
+        facilisis. Magna eget est lorem ipsum dolor sit amet consectetur.
+        Suspendisse sed nisi lacus sed viverra tellus. Fringilla urna porttitor
+        rhoncus dolor purus non enim praesent. Vitae suscipit tellus mauris a
+        diam. Massa tempor nec feugiat nisl. Ut enim blandit volutpat maecenas
+        volutpat. Eleifend donec pretium vulputate sapien nec sagittis. Sagittis
+        nisl rhoncus mattis rhoncus urna neque viverra justo. In iaculis nunc
+        sed augue lacus viverra vitae. Eu consequat ac felis donec et odio. Urna
+        condimentum mattis pellentesque id nibh. Est pellentesque elit
+        ullamcorper dignissim cras tincidunt lobortis. Sed augue lacus viverra
+        vitae congue eu consequat ac. Vivamus arcu felis bibendum ut. Quam nulla
+        porttitor massa id neque aliquam vestibulum. Orci nulla pellentesque
+        dignissim enim sit amet. Eu turpis egestas pretium aenean. Habitant
+        morbi tristique senectus et. Adipiscing tristique risus nec feugiat.
+        Diam sit amet nisl suscipit adipiscing bibendum. Maecenas accumsan lacus
+        vel facilisis volutpat est velit. Ac ut consequat semper viverra nam
+        libero. Lectus vestibulum mattis ullamcorper velit.
+      </p>
     </div>
   );
 }
