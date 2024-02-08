765860cd90a548b72b402fa7c69e540a3dc25e26
BenjaminJedar <benjisaintpaul@hotmail.fr>
2024-02-08 22:28:25 +0100

    Ajout d'un burger menu dans le header en version mobile

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,37 +1,73 @@
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
 import logo_AK from '../../assets/logo_AK_removebg1.png';
 import './header.css';
 
 function Header() {
   const location = useLocation();
+
+  const [burger_class, setBurgerClass] = useState('burger_bar unclicked');
+  const [menu_class, setMenuClass] = useState('menu hidden');
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  // toggle burger menu change
+  function updateMenu() {
+    if (!isMenuClicked) {
+      setBurgerClass('burger_bar clicked');
+      setMenuClass('menu visible');
+    } else {
+      setBurgerClass('burger_bar unclicked');
+      setMenuClass('menu hidden');
+    }
+    setIsMenuClicked(!isMenuClicked);
+  }
+
   return (
-    <div>
-      <div className="headerSection">
-        <Link to="/">
-          <img src={logo_AK} className="logo_AK" alt="logo AK" />
+    <div className="headerSection">
+      <Link to="/">
+        <img src={logo_AK} className="logo_AK" alt="logo AK" />
+      </Link>
+      <nav className="headerNav">
+        <Link
+          to="/a_propos"
+          className={location.pathname === '/a_propos' ? 'lien active' : 'lien'}
+        >
+          Qui suis-je ?
         </Link>
-        <nav className="headerNav">
-          <Link
-            to="/a_propos"
-            className={
-              location.pathname === '/a_propos' ? 'lien active' : 'lien'
-            }
-          >
-            Qui suis-je ?
-          </Link>
-          <Link
-            to="/prestations"
-            className={
-              location.pathname === '/prestations' ? 'lien active' : 'lien'
-            }
-          >
-            Mes préstations
-          </Link>
-          <a href="../#contact" className="lien_contact">
-            Contact
-          </a>
-        </nav>
+        <Link
+          to="/prestations"
+          className={
+            location.pathname === '/prestations' ? 'lien active' : 'lien'
+          }
+        >
+          Mes préstations
+        </Link>
+        <a href="../#contact" className="lien_contact">
+          Contact
+        </a>
+      </nav>
+
+      <div className="mobile_menu">
+        <div className={menu_class}>
+          <nav className="headerNav mobileNav">
+            <Link to="/a_propos" className={'lien lien_mobile'}>
+              Qui suis-je ?
+            </Link>
+            <Link to="/prestations" className={'lien lien_mobile'}>
+              Mes préstations
+            </Link>
+            <a href="../#contact" className="lien_contact lien_mobile">
+              Contact
+            </a>
+          </nav>
+        </div>
+
+        <div className="burger_menu" onClick={updateMenu}>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+        </div>
       </div>
     </div>
   );
