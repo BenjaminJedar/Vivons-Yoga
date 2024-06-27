0e20e985663832862272b1e50811abe44fac3615
BenjaminJedar <benjisaintpaul@hotmail.fr>
2024-06-27 18:08:15 +0200

    fin de la resolution du probleme de conflit de version qui interdisait de push sur le depot distant

--- src/pages/Prestations/prestations.jsx
+++ src/pages/Prestations/prestations.jsx
@@ -4,23 +4,8 @@ const prestation_array = require('../../datas/home_prestations.json');
 
 function Prestations() {
   return (
-<<<<<<< HEAD
     <div className="prestation_page">
       <h2 className="prestation_page_title">Mes préstations</h2>
-=======
-    <div className="presta">
-      <div className="img_container">
-        <img
-          src={cover_img}
-          alt="Ana dans les tournesols"
-          className="img_container"
-        />
-      </div>
-      <h2 className="presta_title">Mes préstations</h2>
-      <div className="presta_card_container">
-        <h3>{prestation_array(1)}</h3>
-      </div>
->>>>>>> bf1b2f912e6d0173b14265e3711bbc6c2878eea7
     </div>
   );
 }
