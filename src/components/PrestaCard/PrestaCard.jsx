15741df980cb1672a34b98c9946452f8fa7d50a3
BenjaminJedar <benjisaintpaul@hotmail.fr>
2024-08-13 19:26:00 +0200

    Mise en forme de la page de prestation

--- src/components/PrestaCard/PrestaCard.jsx
+++ src/components/PrestaCard/PrestaCard.jsx
@@ -6,13 +6,13 @@ function PrestaCard() {
   // Défini l'état pour l'onglet sélectionné
   const [selectedTab, setSelectedTab] = useState(0);
   const [isSelected, setIsSelected] = useState(false);
-  const [tabTitleClass, setTabTitleClass] = useState('tab_container');
+  const [tabTitleClass, setTabTitleClass] = useState('tab_title');
 
   function selectTitle() {
     if (isSelected) {
-      setTabTitleClass('tab_container selected');
+      setTabTitleClass('tab_title selected');
     } else {
-      setTabTitleClass('tab_container');
+      setTabTitleClass('tab_title');
     }
     setIsSelected(!isSelected);
   }
@@ -22,14 +22,17 @@ function PrestaCard() {
   return (
     <div className="presta_card">
       {/* Onglets */}
-      <div className={tabTitleClass}>
+      <div className="tab_container">
         {prestation_data.map((tab, index) => (
           <div
             key={index}
-            className="tab_title"
+            className={
+              selectedTab === index ? 'tab_title selected' : 'tab_title'
+            }
             onClick={() => {
-              selectTitle();
               setSelectedTab(index);
+              selectTitle();
+              console.log('titre n°' + selectedTab + '  ' + 'selectioné');
             }}
           >
             {tab.title}
@@ -38,7 +41,13 @@ function PrestaCard() {
       </div>
 
       {/* Contenu */}
-      <div className="tab_content">{prestation_data[selectedTab].content}</div>
+      <div className="tab_content">
+        {prestation_data[selectedTab].content.map((content, index) => (
+          <p key={index} className="prestation_p">
+            {content}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/Prestations/prestations.jsx
+++ src/pages/Prestations/prestations.jsx
@@ -2,7 +2,6 @@ import './prestations.css';
 import PrestaCard from '../../components/PrestaCard/PrestaCard';
 import img_couverture_prestations from '../../assets/couverture_prestations_redim.jpg';
 import img_couverture_prestations_PC from '../../assets/couverture_prestations_redim_PC.jpg';
-const prestation_array = require('../../datas/home_prestations.json');
 
 function Prestations() {
   return (
@@ -22,15 +21,7 @@ function Prestations() {
       </div>
 
       <h2 className="prestation_page_title">Mes préstations</h2>
-      <PrestaCard />
-      <div className="prestations_container">
-        {prestation_array.map((presta) => (
-          <div className="prestations_description" key={presta.id}>
-            <h3>{presta.title}</h3>
-            <p>{presta.content}</p>
-          </div>
-        ))}
-      </div>
+      <PrestaCard className="presta_card" />
     </div>
   );
 }
